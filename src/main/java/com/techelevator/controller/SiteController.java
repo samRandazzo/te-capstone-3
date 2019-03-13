8ca200d7d4886ecb474e84f70de86f8dd20cd1aa
Joshua Wygonik <jgwygonik@gmail.com>
2019-03-13 10:10:46 -0400

    adding homepage jsp

---
+++ src/main/java/com/techelevator/controller/SiteController.java
@@ -0,0 +1,15 @@
+package com.techelevator.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class SiteController {
+
+	@RequestMapping(path="/", method=RequestMethod.GET)
+	public String displayHomepage() {
+		return "homepage";
+	}
+	
+}
